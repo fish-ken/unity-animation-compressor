68e971bc23a6c18d057a9528c12c202ec24791f1
이상윤 <mousedoc@oozooinc.com>
2021-05-04 11:38:23 +0900

    WIP : Create core

---
+++ Assets/Scenes/Editor/AnimationCompressor/AnimationCompressor.Core.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace AnimationCompressor
+{
+    public class Core
+    {
+        // TODO : Something
+        public void Compress(AnimationClip clip)
+        {
+            if (clip == null)
+            {
+                Debug.LogError("AnimationCompressor.Core.Compress - AnimationClip is null");
+                return;
+            }
+
+            var fileName = $"{clip.name}_Compressed";
+            var compressedClip = Object.Instantiate<AnimationClip>(clip);
+
+            var curves = AnimationUtility.GetAllCurves(clip);
+
+
+            AssetDatabase.CreateAsset((Object)Object.Instantiate<AnimationClip>((M0)animClip), outputPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.DeleteAsset(fileName);
+
+        }
+    }
+}
---
+++ Assets/Scenes/Editor/AnimationCompressor/AnimationCompressor.Editor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace AnimationCompressor
+{
+    public class Editor : EditorWindow
+    {
+
+    }
+}
