6b131d5f16a9573d1f9aae6b473c11b908b78cd3
mousedoc <sangyun3631@gmail.com>
2022-06-02 14:30:52 +0900

    add test source

---
+++ Assets/Editor/AnimationCompressor/Scripts/AnimationCompressor.Core.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AnimationCompressor
+{
+    public class Core
+    {
+        public void Compress(AnimationClip originClip, Option option)
+        {
+            if (originClip == null)
+            {
+                Debug.LogError("AnimationCompressor.Core.Compress - AnimationClip is null");
+                return;
+            }
+
+            var fileName = $"{originClip.name}_Compressed";
+
+            var copyClip = Object.Instantiate<AnimationClip>(originClip);
+
+            var curves = AnimationUtility.GetCurveBindings(originClip);
+
+            var outputPath = GetOutputPath(originClip);
+
+            AssetDatabase.CreateAsset(copyClip, outputPath);
+            AssetDatabase.SaveAssets(); 
+            //AssetDatabase.DeleteAsset(fileName);
+        }
+
+        private string GetOutputPath(AnimationClip originClip)
+        {
+            if (originClip == null)
+                return string.Empty;
+
+
+            var originPath = AssetDatabase.GetAssetPath(originClip);
+            Debug.Log(originPath);
+            return originPath;
+        }
+    }
+}
---
+++ Assets/Editor/AnimationCompressor/Scripts/AnimationCompressor.Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace AnimationCompressor
+{
+    public class Menu
+    {
+        [MenuItem("Tools/Optimization/Animation/Animation Compressor/Open")]
+        public static void OpenMaterialRefCleaner()
+        {
+            ShowWindow();
+        }
+
+        private static void ShowWindow()
+        {
+            var window = EditorWindow.GetWindow<Window>("Anim. Compressor");
+            window.Show();
+        }
+    }
+}
